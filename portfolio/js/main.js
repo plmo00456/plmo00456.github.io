a50f34606d6eca68eb2ef32b11bb8e6ecab7abe9
wj <park_wj7269@naver.com>
2023-12-16 20:48:20 +0900

    다크모드 추가

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,4 +1,25 @@
 window.onload = function(){
+    let theme = 'light';
+
+    const onToggleTheme = ( { currentTarget } ) => {
+        const newTheme = theme === 'light' ? 'dark' : 'light';
+        theme = newTheme;
+
+        document.documentElement.setAttribute( 'data-theme', newTheme );
+        currentTarget.setAttribute( 'aria-pressed', newTheme === 'dark' );
+    };
+
+    // Get switch button and add click event
+    const btn = document.querySelector( `.theme-switch` );
+    if ( ! btn ) return;
+    btn.addEventListener( 'click', onToggleTheme, false );
+
+
+    // DEMO
+    const toggle = document.querySelector( `.writing-toggle` );
+    if ( ! toggle ) return;
+    toggle.addEventListener('click', () => document.body.classList.toggle('vertical'));
+
     let currentSwiper = 0;
     const swiper = new Swiper('.indv-project .images', {
         loop: true,
